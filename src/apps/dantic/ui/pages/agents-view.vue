7d3016f574e09da183a72966170aa94d48d027f4
Delimond-Dev <delimonddev@localhost.localdomain>
2023-01-23 23:47:53 +0100

    update design

--- src/apps/dantic/ui/pages/agents-view.vue
+++ src/apps/dantic/ui/pages/agents-view.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="main-panel">
         <div class="container">
-            <div class="panel-header bg-success-gradient">
-                <div class="page-inner py-5">
+            <div class="panel-header">
+                <div class=" page-inner py-5">
                     <div class="page-header">
                         <h4 class="page-title">Agents</h4>
                         <ul class="breadcrumbs">
                             <li class="nav-home">
-                                <a href="#">
+                                <router-link :to="{ name: 'dashboard-route' }">
                                     <i class="flaticon-home"></i>
-                                </a>
+                                </router-link>
                             </li>
                             <li class="separator">
                                 <i class="flaticon-right-arrow"></i>
@@ -24,9 +24,7 @@
                                 <a href="#">liste des agents</a>
                             </li>
                         </ul>
-                        <router-link :to="{ name: 'agent-create-route' }" class="btn btn-primary btn-sm"> <i
-                                class="flaticon-add"></i>
-                            Nouveau agent</router-link>
+
                     </div>
                 </div>
             </div>
@@ -34,7 +32,70 @@
                 <div class="row mt--2">
                     <div class="col-md-12">
                         <div class="card full-height animated fadeInUp">
+                            <div class="card-header">
+                                <h1 class="card-title pull-left">Liste des agents</h1>
+                                <router-link :to="{ name: 'agent-create-route' }"
+                                    class="btn btn-primary btn-sm pull-right"> <i class="flaticon-add"></i>
+                                    Nouveau agent</router-link>
+                            </div>
                             <div class="card-body">
+                                <div class="table-responsive">
+                                    <table id="agents-datatables" class="display table table-striped table-hover">
+                                        <thead>
+                                            <tr>
+                                                <th>Photo</th>
+                                                <th>Nom complet</th>
+                                                <th>Téléphone</th>
+                                                <th>E-mail</th>
+                                                <th>Adresse</th>
+                                                <th>Territoire</th>
+                                                <th>Province</th>
+                                                <th>Fonction</th>
+                                                <th></th>
+                                            </tr>
+                                        </thead>
+                                        <tfoot>
+                                            <tr>
+                                                <th>Photo</th>
+                                                <th>Nom complet</th>
+                                                <th>Téléphone</th>
+                                                <th>E-mail</th>
+                                                <th>Adresse</th>
+                                                <th>Territoire</th>
+                                                <th>Province</th>
+                                                <th>Fonction</th>
+                                                <th></th>
+                                            </tr>
+                                        </tfoot>
+                                        <tbody>
+                                            <tr v-for="a in 10" :key="a">
+                                                <td>
+                                                    <div class="avatar avatar-sm">
+                                                        <img src="assets/img/jm_denis.jpg" alt="photo"
+                                                            class="avatar-img rounded">
+                                                    </div>
+
+                                                </td>
+
+                                                <td>Gaston Delimond</td>
+                                                <td>+243813719944</td>
+                                                <td>gastondelimond@gmail.com</td>
+                                                <td>03, Bismark, Golf Gombe</td>
+                                                <td>Lukunga</td>
+                                                <td>Kinshasa</td>
+                                                <td>Inspecteur</td>
+                                                <td>
+                                                    <button type="button" data-toggle="tooltip"
+                                                        title=" Voir agent & modification"
+                                                        class="btn btn-link btn-primary btn-lg"
+                                                        data-original-title="Voir agent & modification">
+                                                        <i class="fa fa-edit"></i>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
                                 <!-- table pour afficher les ipa & ita -->
 
                                 <!-- end table -->
@@ -52,6 +113,22 @@
 
 <script>
 export default {
-    name: "Ipa-view"
+    name: "Ipa-view",
+
+    mounted() {
+        $('#agents-datatables').DataTable({
+            "language": {
+                "paginate": {
+                    "previous": "Précédent",
+                    "next": "Suivant"
+                },
+                "lengthMenu": "Afficher _MENU_ enregistrements / page",
+                "zeroRecords": "Aucune données trouvés !",
+                "info": "Afficher _PAGE_ pour _PAGES_",
+                "infoEmpty": "Aucune données à afficher !",
+                "search": "Recherche"
+            }
+        })
+    }
 }
 </script>

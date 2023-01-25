dd3c408b0e94ead4e38cb71e2bb6bd044617b080
Delimond-Dev <delimonddev@localhost.localdomain>
2023-01-25 15:59:12 +0100

    update

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -1,6 +1,54 @@
 <template>
     <div class="login">
-        <div class="wrapper wrapper-login">
+        <div class="wrapper wrapper-login wrapper-login-full p-0">
+            <div
+                class="login-aside w-50 d-flex flex-column align-items-center justify-content-center text-center bg-side-login">
+                <h1 class="title fw-bold text-white mb-3">
+                    <h3 class="text-center"><img src="assets/img/logo_snia.png" class="img-login-snia" alt="SNIA"></h3>
+                </h1>
+            </div>
+            <div class="login-aside w-50 d-flex align-items-center justify-content-center"
+                style="background-color: #efefee;">
+                <div class="container container-login container-transparent animated fadeIn bg-white">
+                    <h3 class="text-center fw-bold mb-3 title">Authentification</h3>
+                    <p class="text-center small text-info" style="font-style: italic;">Veuillez entrer vos identifiants
+                        pour vous
+                        connecter !</p>
+                    <form class="needs-validation" id="form-login" @submit.prevent="loggedIn" novalidate>
+                        <div class="login-form">
+                            <div class="form-group form-floating-label">
+                                <input id="username" v-model="user.email" name="username" type="text"
+                                    class=" form-control input-border-bottom" required>
+                                <label for="username" class="placeholder text-color-snia">Identifiant</label>
+                                <div class="invalid-feedback">
+                                    votre idantifiant est requis !
+                                </div>
+                            </div>
+                            <div class="form-group form-floating-label">
+                                <input id="password" name="password" v-model="user.password" :type="inputType"
+                                    class="form-control input-border-bottom" required>
+                                <label for="password" class="placeholder text-color-snia">Mot de passe</label>
+                                <div class="show-password" @click="toggleVisibility">
+                                    <i class="icon-eye"></i>
+                                </div>
+                                <div class="invalid-feedback">
+                                    mot de passe requis pour se connecter !
+                                </div>
+                            </div>
+                            <div class=" row form-sub m-0">
+                                <a href="#" class="link  float-right ">Mot de passe perdu?</a>
+                            </div>
+                            <div class="form-action mb-3">
+                                <button type="submit" id="btn-login" class="btn btn-snia  btn-full btn-login">
+                                    <i class="icon-login"></i> &nbsp;
+                                    Connexion</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- <div class="wrapper wrapper-login">
             <div class="container container-login animated fadeIn">
                 <h3 class="text-center"><img src="assets/img/logo_snia.png" class="img-login-snia" alt="SNIA"></h3>
                 <form class="needs-validation" id="form-login" @submit.prevent="loggedIn" novalidate>
@@ -35,7 +83,7 @@
                     </div>
                 </form>
             </div>
-        </div>
+        </div> -->
     </div>
 </template>
 <script>

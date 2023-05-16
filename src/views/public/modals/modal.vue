3271769a8ba92d82b0a587758a500976e957299c
Chris Tenday <ctenday@gmail.com>
2023-05-17 00:30:56 +0100

    working form sous champs 2-level

---
+++ src/views/public/modals/modal.vue
@@ -0,0 +1,53 @@
+<template>
+    <teleport to='body'>
+        <div class="modal fade" id="agentModal" tabindex="-1" role="dialog" aria-labelledby="agentModalLabel"
+             aria-hidden="true">
+            <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable" role="document">
+                <div class="modal-content">
+                    <div class="modal-header bg-app-2">
+                        <h5 class="modal-title text-white fw-mediumbold" id="agentModalLabel">
+                            <i class="fa fa-user-plus mr-2"></i>
+                            CRÉATION NOUVEAU AGENT
+                        </h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true" class="text-white">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <!-- section reservée pour le chargement du body du modal !-->
+                        <slot name="body"></slot>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </teleport>
+</template>
+
+<script>
+    export default {
+        name: "modal",
+        props:{
+            display:{
+                type:Boolean,
+                default:true
+            }
+        },
+        watch:{
+            display(newVal)
+            {
+                if(newVal===true)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>

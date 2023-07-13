c7ed22397f47ea298d49b44a293fcb09acbe0d3c
unknown <Delimond-eng>
2023-07-13 13:09:37 +0200

    latest update

--- src/components/bs4/src/bs_modal.vue
+++ src/components/bs4/src/bs_modal.vue
@@ -17,7 +17,7 @@
 
                     </perfect-scrollbar>
                     <!-- Modal footer -->
-                    <div class="modal-footer" v-show="$slots.footerContent">
+                    <div class="modal-footer">
                         <slot name="footer-content"></slot>
                     </div>
                 </div>

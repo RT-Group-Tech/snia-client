40441880a3ddb399aca524ad9696b0d24ddf933e
Chris Tenday <ctenday@gmail.com>
2023-11-14 18:16:01 +0100

    new release

--- src/apps/dantic/ui/pages/agents-page.vue
+++ src/apps/dantic/ui/pages/agents-page.vue
@@ -121,12 +121,6 @@ export default {
           var path="/#/dantic/user/profile/view";
           var goTo=domain+path;
 
-          //this.$router.replace({ name: "dantic-profile-route" });
-          /**this.$router.push({path:"/dantic/user/profile/view"})
-          .then(()=>{
-            this.$router.go();
-          }); *///TODO: got to route by reloading browser.
-
           this.$router.push({ path: '/dantic/user/profile/view' })
               .then(() => { this.$router.go() })
         }
